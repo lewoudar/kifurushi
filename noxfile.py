73338a4a72097b84f4bd42598ed4a77eea53ffa7
le_woudar <rollandkev@yahoo.fr>
2023-11-27 19:31:45 +0100

    ci: updated GitHub action files and noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -5,7 +5,7 @@ import nox
 
 nox.options.reuse_existing_virtualenvs = True
 
-PYTHON_VERSIONS = ['pypy3', '3.7', '3.8', '3.9', '3.10', '3.11']
+PYTHON_VERSIONS = ['pypy3', '3.8', '3.9', '3.10', '3.11', '3.12']
 CI_ENVIRONMENT = 'GITHUB_ACTIONS' in os.environ
 
 
@@ -13,33 +13,30 @@ CI_ENVIRONMENT = 'GITHUB_ACTIONS' in os.environ
 def lint(session):
     """Performs pep8 and security checks."""
     source_code = 'kifurushi'
-    session.install('flake8==3.9.2', 'bandit==1.7.4', 'black==22.3.0')
-    session.run('flake8', source_code)
+    session.run('poetry', 'install', '--only', 'lint')
+    session.run('ruff', 'check', source_code)
+    session.run('ruff', 'format', '.')
     session.run('bandit', '-r', source_code)
-    session.run('black', source_code, 'tests', '--check')
 
 
 @nox.session(python=PYTHON_VERSIONS[-1])
 def safety(session):
     """Checks vulnerabilities of the installed packages."""
-    session.install('poetry>=1.0.0,<1.3.0')
-    session.run('poetry', 'install')
+    session.run('poetry', 'install', '--only', 'audit')
     session.run('safety', 'check')
 
 
 @nox.session(python=PYTHON_VERSIONS)
 def tests(session):
     """Runs the test suite."""
-    session.install('poetry>=1.0.0,<1.3.0')
-    session.run('poetry', 'install')
+    session.run('poetry', 'install', '--with', 'test')
     session.run('pytest')
 
 
 @nox.session(python=PYTHON_VERSIONS[-1])
 def docs(session):
     """Builds the documentation."""
-    session.install('poetry>=1.0.0,<1.3.0')
-    session.run('poetry', 'install')
+    session.run('poetry', 'install', '--only', 'docs')
     session.run('mkdocs', 'build', '--clean')
 
 
@@ -48,7 +45,6 @@ def deploy(session):
     """
     Deploys on pypi.
     """
-    session.install('poetry>=1.0.0,<1.3.0')
     if 'POETRY_PYPI_TOKEN_PYPI' not in os.environ:
         session.error('you must specify your pypi token api to deploy your package')
 
